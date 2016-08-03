5f36fd3a3830601c4a89be21f5466f2f3e3e0813
Willem Dehaes <willemdehaes@gmail.com>
2016-08-03 20:37:04 +0000

    including grams in db (7)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "grams#index"
+  resources :grams, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
