e5ec2365ef93fce0d29e24e4a05575acbcd2cc82
Willem Dehaes <willemdehaes@gmail.com>
2016-08-05 21:20:57 +0000

    13 part 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "grams#index"
-  resources :grams, only: [:new, :create, :show, :edit]
+  resources :grams, only: [:new, :create, :show, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,6 +2,29 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
 
+  describe "grams#update" do
+    it "should allow users to succesfully update gems" do
+      p = FactoryGirl.create(:gram, message: "Initial Value")
+      patch :update, id: p.id, gram: { message: 'Changed'}
+      expect(response).to redirect_to root_path
+      p.reload
+      expect(p.message).to eq "Changed"
+    end
+
+    it "should have http 404 error if the gram cannot be found" do
+      patch :update, id: "BLABLA", gram: {message: 'Changed'}
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "should render the edit form with an http status of unprocessable_entity" do
+       p = FactoryGirl.create(:gram, message: "Initial Value")
+       patch :update, id: p.id, gram: {message: ''}
+       expect(response).to have_http_status(:unprocessable_entity)
+       p.reload
+       expect(p.message).to eq "Initial Value"     
+    end
+  end
+
   describe "grams#edit" do
     it "should succesfully show the edit form if the gram is found" do
       p = FactoryGirl.create(:gram)
